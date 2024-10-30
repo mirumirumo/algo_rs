c17ee99e798e675640ab317e57b44ef0ac294cb9
mirumirumo <yoshinouchi.m.aa@gmail.com>
2024-10-30 22:17:25 +0900

    UnionFindの実装

--- src/bin/union_find/main.rs
+++ src/bin/union_find/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 #[derive(Clone)]
 pub struct UnionFind {
     parent: Vec<usize>,
@@ -9,6 +11,34 @@ impl UnionFind {
         UnionFind {
             parent: (0..n).collect::<Vec<_>>(),
             rank: vec![0; n],
+        }
+    }
+
+    pub fn is_root(&self, x: usize) -> bool {
+        self.parent[x] == x
+    }
+
+    pub fn find(&mut self, x: usize) -> usize {
+        if self.is_root(x) {
+            x
+        } else {
+            let root = self.find(self.parent[x]);
+            self.parent[x] = root;
+            root
+        }
+    }
+
+    pub fn union(&mut self, x: usize, y: usize) {
+        let (root_x, root_y) = (self.find(x), self.find(y));
+        if root_x != root_y {
+            match self.rank[root_x].cmp(&self.rank[root_y]) {
+                Ordering::Less => self.parent[root_x] = root_y,
+                Ordering::Equal => {
+                    self.parent[root_y] = root_x;
+                    self.rank[root_y] += 1;
+                }
+                Ordering::Greater => self.parent[root_y] = root_x,
+            }
         }
     }
 }
