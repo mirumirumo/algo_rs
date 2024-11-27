3dc92c5d36c1d6c08a84590a84627645ac6ec7ca
mirumirumo <yoshinouchi.m.aa@gmail.com>
2024-11-27 22:02:49 +0900

    add some funcs

--- src/container/queue.rs
+++ src/container/queue.rs
@@ -25,10 +25,37 @@ impl<T> Deque<T> {
         }
         self.buffer[self.end] = Some(x);
         self.size += 1;
-        self.end += 1;
-        if self.end == self.buffer.len() {
-            self.end = 0;
-        }
+        self.end = (self.end + 1) % self.buffer.len();
         return true;
+    }
+    pub fn push_front(&mut self, x: T) -> bool {
+        if self.buffer.len() >= self.size {
+            return false;
+        }
+        self.front = (self.front - 1) % self.buffer.len();
+        self.buffer[self.front] = Some(x);
+        self.size += 1;
+        return true;
+    }
+    pub fn pop_back(&mut self) -> Option<&T> {
+        todo!()
+    }
+    pub fn pop_front(&mut self) -> Option<&T> {
+        todo!()
+    }
+    pub fn front(&self) -> Option<&T> {
+        todo!()
+    }
+    pub fn back(&self) -> Option<&T> {
+        todo!()
+    }
+    pub fn len(&self) -> usize {
+        todo!()
+    }
+    pub fn is_empty(&self) -> bool {
+        todo!()
+    }
+    pub fn clear(&mut self) {
+        todo!()
     }
 }
